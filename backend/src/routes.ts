4faf046d73df1a4d96ff642a2ed0cc5918ccccce
Bruno Ferraz Mazzoni <bruno.bmxdabiciceta@gmaol.com>
2023-07-20 21:55:35 -0300

    tratando os erros

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,8 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ nome: 'sujeito pizza' })
+        return res.json({ nome: 'sujeito pizza' })
+        /* throw new Error('Erro ao fazer essa requisição') */
 })
 
 export { router };
